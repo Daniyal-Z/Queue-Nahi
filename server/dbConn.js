f796bb0ebb969e55b93bf90df1deb797c0ba52b2
Daniyal-Z <62768256+Daniyal-Z@users.noreply.github.com>
2025-04-26 16:35:01 +0500

    Updated dbConn file for security

--- server/dbConn.js
+++ server/dbConn.js
@@ -2,15 +2,15 @@ const sql = require('mssql');
 
 //the server and login info needed to connect with sql server
 const config = {
-    user: "sa",
-    password: "1729314",
-    server: "DESKTOP-72HCC7I",
+    user: "NAME",
+    password: "PASS",
+    server: "SERVER",
     database: "QNahiDB",
     options: {
         trustServerCertificate: true,
         trustedConnection: false,
         enableArithAbort: true,
-        instancename: "SQL_DB"
+        instancename: "INSTANCE NAME"
     },
     port: 1433   //for now is 1433 but if TCP port is changed in config of IAPII then here asw
 };
